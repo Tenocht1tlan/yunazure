27d3e7d712a752f0def981c483890dfa5177ce67
CheerDay <445726436@qq.com>
2020-06-04 17:29:13 +0800

    yxh
    star +++

--- pages/star/star.js
+++ pages/star/star.js
@@ -1,9 +1,13 @@
-
+// pages/register/index.js
+let app = getApp();
+//获取云数据库引用
+const db = wx.cloud.database();
 Page({
 
   data: {
     isEdit:false,
-    Edit:'编辑'
+    Edit:'编辑',
+    goods:[]
   },
 
   edit:function(e){
@@ -18,5 +22,13 @@ Page({
         Edit:temp,
         isEdit:edit
       })
-     }
+     },
+
+
 })
+// db.collection('goods').doc('_id_').get({
+//   success: function(res) {
+//     // res.data 包含该记录的数据
+//     console.log(res.data)
+//   }
+// })
