7dc0769db34fe34973806af81fac618901ededee
CheerDay <445726436@qq.com>
2020-06-08 22:04:44 +0800

    yxh
    index unfinshed favorite can not add in

---
+++ functions/addFav/index.js
@@ -0,0 +1,24 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+const _ = db.command
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  try {
+    return await db.collection('favorite').where({
+      _openid:wxContext.OPENID,
+    }).update({
+      data: {
+        items: _.push({
+          each: event.items
+        })
+        // _.push(event.items)
+      }
+    })
+  } catch(e) {
+    console.error(e)
+  }
+}
