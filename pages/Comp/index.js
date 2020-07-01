a260598c6b56020adad8fe0a1e1d7ecb1267f6c4
CheerDay <445726436@qq.com>
2020-07-01 22:27:45 +0800

    yxh
    ad

--- pages/Comp/index.js
+++ pages/Comp/index.js
@@ -1,4 +1,5 @@
-// pages/Comp/index.js
+// pages/opinion/index.js
+const db =  wx.cloud.database();
 Page({
 
   /**
@@ -6,15 +7,35 @@ Page({
    */
   data: {
 
+    swiperHeight:1000
   },
 
+  
+  commit:function(){
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.initBanners()
   },
-
+  async initBanners(){
+    const _data = {}
+    // 读取头部轮播图
+    db.collection('image').get().then(res=>{
+      if(res.data){
+        _data.banners = res.data
+        this.setData(_data)
+      }else{
+        wx.showModal({
+          title: '提示',
+          content: 'error',
+          showCancel: false
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
